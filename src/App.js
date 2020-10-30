146b4a6688ff768e714351f8ff2fef5dd3a52403
malikfayyaz22 <malikfayyaz22@gmail.com>
2020-10-31 00:39:50 +0930

    updated app.js source code

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>Updated By Fayyaz ahmad for my Paco boy</p>
         </p>
         <a
           className="App-link"
